d8591aabfe60ded92786e438aed9d7e2a5afe8bc
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-04-13 16:11:12 +0200

    CS fixes - native_function_invocation & static_lambda

--- Tests/BandwidthTransportTest.php
+++ Tests/BandwidthTransportTest.php
@@ -61,7 +61,7 @@ final class BandwidthTransportTest extends TransportTestCase
     public function testNoInvalidArgumentExceptionIsThrownIfFromIsValid(string $from)
     {
         $message = new SmsMessage('+33612345678', 'Hello!');
-        $client = new MockHttpClient(function (string $method, string $url): ResponseInterface {
+        $client = new MockHttpClient(static function (string $method, string $url): ResponseInterface {
             self::assertSame('POST', $method);
             self::assertSame('https://messaging.bandwidth.com/api/v2/users/account_id/messages', $url);
 
